b67407ec081aa54857ad604395267afc0d91494b
AlejandroCRG <alejandrocrg1@gmail.com>
2022-12-08 17:49:13 +0100

    need to automatize 16** when starting

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+let gridbtn = document.createElement('button')
+let body = document.querySelector('body')
 let divcontainer = document.querySelector('.grid-container')
+setSize(16)
 
 
-let i = 1;
-while (i <= 16){
-    let div = document.createElement('div');
-    div.setAttribute('id', `${i}`);
-    div.setAttribute('class', `grid`);
-    divcontainer.appendChild(div);
-    i++;
+
+function setSize(input){
+    let gridarrays = document.querySelectorAll('.grid')
+    gridarrays.forEach((grid) => grid.remove())
+    input = prompt('Set size brudda')
+    divcontainer.style.gridTemplateColumns = `repeat(${input}, 1fr)`
+    divcontainer.style.gridTemplateRows = `repeat(${input}, 1fr)`
+    let value = input * input
+    if (input <= 100){
+        for (let i = 1; i <= value; i++){
+            let div = document.createElement('div');
+            div.setAttribute('id', `${i}`);
+            div.setAttribute('class', `grid`);
+            divcontainer.appendChild(div);
+        }
+    }
+    else{
+        console.log('error')
+    }
 }
 
-let gridarrays = document.querySelectorAll('.grid')
-
-console.log(gridarrays)
+let button = document.querySelector('.sizebtn')
+button.addEventListener('click', function(e){
+    setSize()
+})
